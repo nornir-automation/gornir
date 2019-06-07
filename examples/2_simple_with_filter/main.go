f3eecfa907a1589303355d50529bb363030ee4d9
Nicolas Leiva <nicolasleiva@gmail.com>
2019-06-07 14:04:05 -0400

    Add new build options

--- examples/2_simple_with_filter/main.go
+++ examples/2_simple_with_filter/main.go
@@ -14,22 +14,23 @@ import (
 )
 
 func main() {
-	logger := logger.NewLogrus(false)
+	// Instantiate a logger plugin
+	log := logger.NewLogrus(false)
 
-	inventory, err := inventory.FromYAMLFile("/go/src/github.com/nornir-automation/gornir/examples/hosts.yaml")
+	// Load the inventory using the FromYAMLFile plugin
+	file := "/go/src/github.com/nornir-automation/gornir/examples/hosts.yaml"
+	plugin := inventory.FromYAML{HostsFile: file}
+	inv, err := plugin.Create()
 	if err != nil {
-		logger.Fatal(err)
-	}
-
-	gr := &gornir.Gornir{
-		Inventory: inventory,
-		Logger:    logger,
+		log.Fatal(err)
 	}
 
 	// define a function we will use to filter the hosts
 	filter := func(ctx context.Context, h *gornir.Host) bool {
 		return h.Hostname == "dev1.group_1" || h.Hostname == "dev4.group_2"
 	}
+
+	gr := gornir.New().WithInventory(inv).WithLogger(log).WithFilter(filter)
 
 	// Before calling Gornir.RunS we call Gornir.Filter and pass the function defined
 	// above. This will narrow down the inventor to the hosts matching the filter
@@ -39,7 +40,7 @@ func main() {
 		&task.RemoteCommand{Command: "ip addr | grep \\/24 | awk '{ print $2 }'"},
 	)
 	if err != nil {
-		logger.Fatal(err)
+		log.Fatal(err)
 	}
 
 	output.RenderResults(os.Stdout, results, true)
--- pkg/gornir/gornir.go
+++ pkg/gornir/gornir.go
@@ -1,4 +1,5 @@
-// Package Gornir implements the core functionality and define the needed interfaces to integrate with the framework
+// Package gornir implements the core functionality and define the needed
+// interfaces to integrate with the framework
 package gornir
 
 import (
@@ -14,6 +15,41 @@ import (
 type Gornir struct {
 	Inventory *Inventory // Inventory for the object
 	Logger    Logger     // Logger for the object
+}
+
+// New is a Gornir constructor. It is currently no different that new,
+// however is a placeholder for any future defaults.
+func New() *Gornir {
+	return new(Gornir)
+}
+
+// InventoryPlugin is an Inventory Source
+type InventoryPlugin interface {
+	Create() (Inventory, error)
+}
+
+// WithInventory creates a new Gornir with an Inventory.
+func (gr *Gornir) WithInventory(inv Inventory) *Gornir {
+	return &Gornir{
+		Logger:    gr.Logger,
+		Inventory: &inv,
+	}
+}
+
+// WithFilter creates a new Gornir with a filtered Inventory.
+func (gr *Gornir) WithFilter(f FilterFunc) *Gornir {
+	return &Gornir{
+		Logger:    gr.Logger,
+		Inventory: gr.Inventory.Filter(context.TODO(), f),
+	}
+}
+
+// WithLogger creates a new Gornir with a Logger.
+func (gr *Gornir) WithLogger(l Logger) *Gornir {
+	return &Gornir{
+		Logger:    l,
+		Inventory: gr.Inventory,
+	}
 }
 
 // Filter filters the hosts in the inventory returning a copy of the current
