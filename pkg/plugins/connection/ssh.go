08ec4359db895ff1a1d1b3380e291813396959fc
Daniel Justice <djustice@wificidr.net>
2020-07-09 10:27:16 -0500

    Remove SSH config parser and refactor for customizable client config func

--- pkg/plugins/connection/ssh.go
+++ pkg/plugins/connection/ssh.go
@@ -3,14 +3,9 @@ package connection
 import (
 	"context"
 	"fmt"
-	"io/ioutil"
-	"os/user"
-	"path/filepath"
-	"strings"
 
 	"github.com/nornir-automation/gornir/pkg/gornir"
 
-	"github.com/kevinburke/ssh_config"
 	"github.com/pkg/errors"
 	"golang.org/x/crypto/ssh"
 )
@@ -37,7 +32,13 @@ func (s SSH) String() string {
 
 // SSHOpen is a Connection plugin that opens a connection with a device
 type SSHOpen struct {
-	Meta *gornir.TaskMetadata // Task metadata
+	Meta           *gornir.TaskMetadata                                         // Task metadata
+	ClientConfigFn func(*gornir.Host, gornir.Logger) (*ssh.ClientConfig, error) // SSH client configuration
+}
+
+// UserAuthMethods allow the user to define their own SSH auth methods
+type UserAuthMethods interface {
+	SSHAuthMethods() *[]ssh.AuthMethod
 }
 
 // Metadata returns the task metadata
@@ -45,61 +46,24 @@ func (t *SSHOpen) Metadata() *gornir.TaskMetadata {
 	return t.Meta
 }
 
-type authMethodResult struct {
-	authMethods []ssh.AuthMethod
-	err         error
-}
-
-func getAuthMethods(host *gornir.Host, logger gornir.Logger) (*[]ssh.AuthMethod, error) {
-	var authMethods = []ssh.AuthMethod{ssh.Password(host.Password)}
-	// GetStrict should return a default value per `man ssh_config` if this fails, it is because we couldn't parse the config file
-	sshPrivKeyFname, err := ssh_config.GetStrict(host.Hostname, "IdentityFile")
-	if err != nil {
-		return nil, err
-	}
-	signer, err := getPubKeySigner(host, sshPrivKeyFname, logger)
-	// Drop private key auth from the list and fallback to user/pass
-	if err != nil {
-		return &authMethods, nil
-	}
-	authMethods = append(authMethods, ssh.PublicKeys(*signer))
-	return &authMethods, nil
-}
-
-func getPubKeySigner(host *gornir.Host, sshPrivKeyFname string, logger gornir.Logger) (*ssh.Signer, error) {
-	usr, _ := user.Current()
-	homeDir := usr.HomeDir
-	if sshPrivKeyFname == "~" {
-		sshPrivKeyFname = homeDir
-	} else if strings.HasPrefix(sshPrivKeyFname, "~/") {
-		sshPrivKeyFname = filepath.Join(homeDir, sshPrivKeyFname[2:])
-	}
-	key, err := ioutil.ReadFile(sshPrivKeyFname)
-	if err != nil {
-		logger.Debug(fmt.Sprintf("unable to read private key: %v", err))
-		return nil, err
-	}
-
-	// Create the Signer for this private key.
-	signer, err := ssh.ParsePrivateKey(key)
-	if err != nil {
-		logger.Error(fmt.Sprintf("unable to parse private key: %v", err))
-		return nil, err
-	}
-	return &signer, nil
-}
-
 // Run implements gornir.Task interface
 func (t *SSHOpen) Run(ctx context.Context, logger gornir.Logger, host *gornir.Host) (gornir.TaskInstanceResult, error) {
-	authMethods, err := getAuthMethods(host, logger)
-	if err != nil {
-		return &SSH{}, err
+	var sshConfig *ssh.ClientConfig
+	if t.ClientConfigFn != nil { // The client specified a config
+		config, err := t.ClientConfigFn(host, logger)
+		if err != nil {
+			return nil, err
+		}
+		sshConfig = config
+	} else { // Running with defaults
+		sshConfig = &ssh.ClientConfig{
+			User: host.Username,
+			Auth: []ssh.AuthMethod{
+				ssh.Password(host.Password),
+			},
+			HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		} // #nosec
 	}
-	sshConfig := &ssh.ClientConfig{
-		User:            host.Username,
-		Auth:            *authMethods,
-		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-	} // #nosec
 	port := host.Port
 	if port == 0 {
 		port = 22
