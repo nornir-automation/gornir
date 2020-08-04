c2337246a184ac29264f97250f7ece9c63e1e8ae
Daniel <dgjustice@users.noreply.github.com>
2020-08-04 02:31:06 -0500

    Allow using a callback function to generate SSHConfig object when using SSH connection plugin (#51)

--- pkg/plugins/connection/ssh.go
+++ pkg/plugins/connection/ssh.go
@@ -30,9 +30,13 @@ func (s SSH) String() string {
 	return "  - connection opened"
 }
 
+// ClientConfigFn is an interface that allows users to implement their own SSH auth mechanisms
+type ClientConfigFn func(*gornir.Host, gornir.Logger) (*ssh.ClientConfig, error)
+
 // SSHOpen is a Connection plugin that opens a connection with a device
 type SSHOpen struct {
-	Meta *gornir.TaskMetadata // Task metadata
+	Meta           *gornir.TaskMetadata // Task metadata
+	ClientConfigFn ClientConfigFn       // SSH client configuration
 }
 
 // Metadata returns the task metadata
@@ -40,20 +44,32 @@ func (t *SSHOpen) Metadata() *gornir.TaskMetadata {
 	return t.Meta
 }
 
-// Run implements gornir.Task interface
-func (t *SSHOpen) Run(ctx context.Context, logger gornir.Logger, host *gornir.Host) (gornir.TaskInstanceResult, error) {
-	sshConfig := &ssh.ClientConfig{
+// defaultSSHClientConfig implements ClientConfigFn
+func defaultSSHClientConfig(host *gornir.Host, logger gornir.Logger) (*ssh.ClientConfig, error) {
+	return &ssh.ClientConfig{
 		User: host.Username,
 		Auth: []ssh.AuthMethod{
 			ssh.Password(host.Password),
 		},
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-	} // #nosec
+	}, nil // #nosec
+}
+
+// Run implements gornir.Task interface
+func (t *SSHOpen) Run(ctx context.Context, logger gornir.Logger, host *gornir.Host) (gornir.TaskInstanceResult, error) {
+	clientConfigFn := defaultSSHClientConfig
+	if t.ClientConfigFn != nil { // The client specified a config
+		clientConfigFn = t.ClientConfigFn
+	}
+	config, err := clientConfigFn(host, logger)
+	if err != nil {
+		return &SSH{}, errors.Wrap(err, "failed to build SSH client configuration")
+	}
 	port := host.Port
 	if port == 0 {
 		port = 22
 	}
-	client, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", host.Hostname, port), sshConfig)
+	client, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", host.Hostname, port), config)
 	if err != nil {
 		return &SSH{}, errors.Wrap(err, "failed to dial")
 	}
