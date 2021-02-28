174c1c3e01cea931261dc884c083d85521980692
Patrick Ogenstad <patrick@ogenstad.com>
2021-02-28 15:59:48 +0100

    Correct outdated documentation (#52)
    Fixes #43

--- doc.go
+++ doc.go
@@ -7,9 +7,11 @@
 // 	package main
 //
 // 	import (
+// 		"context"
 // 		"os"
 //
 // 		"github.com/nornir-automation/gornir/pkg/gornir"
+// 		"github.com/nornir-automation/gornir/pkg/plugins/connection"
 // 		"github.com/nornir-automation/gornir/pkg/plugins/inventory"
 // 		"github.com/nornir-automation/gornir/pkg/plugins/logger"
 // 		"github.com/nornir-automation/gornir/pkg/plugins/output"
@@ -30,44 +32,60 @@
 // 		gr := gornir.New().WithInventory(inv).WithLogger(log).WithRunner(runner.Parallel())
 //
 // 		results, err := gr.RunSync(
-// 			"What's my ip?",
+// 			context.Background(),
+// 			&connection.SSHOpen{},
+// 		)
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+//
+// 		// defer closing the SSH connection we just opened
+// 		defer func() {
+// 			results, err = gr.RunSync(
+// 				context.Background(),
+// 				&connection.SSHClose{},
+// 			)
+// 			if err != nil {
+// 				log.Fatal(err)
+// 			}
+// 		}()
+//
+// 		results, err = gr.RunSync(
+// 			context.Background(),
 // 			&task.RemoteCommand{Command: "ip addr | grep \\/24 | awk '{ print $2 }'"},
 // 		)
 // 		if err != nil {
 // 			log.Fatal(err)
 // 		}
-// 		output.RenderResults(os.Stdout, results, true)
+// 		output.RenderResults(os.Stdout, results, "What is my ip?", true)
 // 	}
 //
 // would render:
 //
-//     # What's my ip?
+//     # What is my ip?
 //     @ dev5.no_group
-//       - err: failed to dial: ssh: handshake failed: ssh: unable to authenticate, attempted methods [none password], no supported methods remain
+//       - err: failed to retrieve connection: couldn't find connection
 //
 //     @ dev1.group_1
-//      * Stdout: 10.21.33.101/24
+//       - stdout: 10.21.33.101/24
 //
-//      * Stderr:
-//       - err: <nil>
+//       - stderr:
+//     @ dev6.no_group
+//       - stdout: 10.21.33.106/24
 //
-//     @ dev2.group_1
-//      * Stdout: 10.21.33.102/24
-//
-//      * Stderr:
-//       - err: <nil>
-//
-//     @ dev3.group_2
-//      * Stdout: 10.21.33.103/24
-//
-//      * Stderr:
-//       - err: <nil>
-//
+//       - stderr:
 //     @ dev4.group_2
-//      * Stdout: 10.21.33.104/24
+//       - stdout: 10.21.33.104/24
 //
-//      * Stderr:
-//       - err: <nil>
+//       - stderr:
+//     @ dev3.group_2
+//       - stdout: 10.21.33.103/24
+//
+//       - stderr:
+//     @ dev2.group_1
+//       - stdout: 10.21.33.102/24
+//
+//       - stderr:
 //
 // You can see more examples here: https://github.com/nornir-automation/gornir/tree/master/examples
 package gornir
